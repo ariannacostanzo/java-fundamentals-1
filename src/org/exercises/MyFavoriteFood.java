0576b42b31470cf8855821f83ae28874a1307b46
ariannacostanzo <costanzo.arianna@outlook.it>
2024-05-15 14:42:10 +0200

    finished MyFavoriteFood

--- src/org/exercises/MyFavoriteFood.java
+++ src/org/exercises/MyFavoriteFood.java
@@ -1,7 +1,15 @@
 package org.exercises;
 
+
 public class MyFavoriteFood {
     public static void main(String[] args) {
 
+        String[] myFavoriteFood = {"Pizza", "Carbonara", "Sushi", "Hamburger", "Polpette"};
+
+        //lunghezza
+        System.out.println("La mia classifica è lunga: " + myFavoriteFood.length);
+        System.out.println("Il mio cibo preferito è: " + myFavoriteFood[0]);
+        System.out.println("Il cibo che meno preferisco è: " + myFavoriteFood[myFavoriteFood.length - 1]);
+        System.out.println("Il cibo che mi piace mediamente è: " + myFavoriteFood[myFavoriteFood.length/2]);
     }
 }
