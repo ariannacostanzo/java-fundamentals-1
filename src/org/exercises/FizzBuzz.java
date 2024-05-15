adecee08f4687afe7eadfc6f0de7d1bf7168f5be
ariannacostanzo <costanzo.arianna@outlook.it>
2024-05-15 14:08:38 +0200

    first commit

---
+++ src/org/exercises/FizzBuzz.java
@@ -0,0 +1,7 @@
+package org.exercises;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/exercises/MyFavoriteFood.java
@@ -0,0 +1,7 @@
+package org.exercises;
+
+public class MyFavoriteFood {
+    public static void main(String[] args) {
+
+    }
+}
